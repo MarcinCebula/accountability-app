a5e13346344fe5f8a47ee96c68a9eee1ee0f58bf
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2016-11-19 17:39:00 -0500

    sdf

--- app/controllers/twillio_controller.rb
+++ app/controllers/twillio_controller.rb
@@ -2,33 +2,33 @@ class TwillioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def sms
-    # @twillio = Twilio::REST::Client.new
-    # if params['Body'].downcase.includes?('yes')
-    #   @twillio.messages.create(
-    #                            from: '+13472692547',
-    #                            to: params['From'],
-    #                            body: 'Good Job'
-    #                            )
-    # end
-    # if params['Body'].downcase.includes?('noo')
-    #   @twillio.messages.create(
-    #                            from: '+13472692547',
-    #                            to: params['From'],
-    #                            body: 'Please Remember to go on Friday at 6pm'
-    #                            )
-    # end
-    # if params['Body'].downcase.includes?('no')
-    #   @twillio.messages.create(
-    #                            from: '+13472692547',
-    #                            to: params['From'],
-    #                            body: 'You missed your activity 2 times in a row. You friend will be notified'
-    #                            )
+    @twillio = Twilio::REST::Client.new
+    if params['Body'].downcase.includes?('yes')
+      @twillio.messages.create(
+                               from: '+13472692547',
+                               to: params['From'],
+                               body: 'Good Job'
+                               )
+    end
+    if params['Body'].downcase.includes?('noo')
+      @twillio.messages.create(
+                               from: '+13472692547',
+                               to: params['From'],
+                               body: 'Please Remember to go on Friday at 6pm'
+                               )
+    end
+    if params['Body'].downcase.includes?('no')
+      @twillio.messages.create(
+                               from: '+13472692547',
+                               to: params['From'],
+                               body: 'You missed your activity 2 times in a row. You friend will be notified'
+                               )
 
-    #   User.where(phone: params['From'][-2..-1]).first.friends.each do |friend|
-    #     from: '+13472692547',
-    #     to: "+1#{frined.phone}",
-    #     body: "You friend didn't go to the gym 2 times. inform him"
-    #   end
-  # end
+      User.where(phone: params['From'][-2..-1]).first.friends.each do |friend|
+        from: '+13472692547',
+        to: "+1#{frined.phone}",
+        body: "You friend didn't go to the gym 2 times. inform him"
+      end
+    end
   end
 end
