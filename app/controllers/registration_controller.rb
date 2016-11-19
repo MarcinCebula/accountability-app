6af0c587a3ba7ac68dd099786efa1731b111ba8e
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2016-11-19 00:42:00 -0500

    Inital Registrations View

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,4 @@
+class RegistrationController < ApplicationController
+  def phone_number
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#hello"
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
+
+  resources :registration, only: [:index] do
+    get 'phone-number', as: :phonenumber, on: :collection
+  end
+  root to: "registration#phone_number"
 end
